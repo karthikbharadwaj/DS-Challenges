d754a18df5a8f68922f333e0965417da3b878a49
Kartihk Bharadwaj <karthikb@sg.ibm.com>
2015-03-31 14:47:51 +0800

    Adding feature selection module

--- Otto/pre_process.py
+++ Otto/pre_process.py
@@ -4,14 +4,17 @@ __authors__ = "Ganesh Karthik"
 
 import pandas as pd
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.cross_validation import train_test_split
 
 
 def remove_zero_var(df):
     '''
     The function removes columns with zero variance columns
+    Removes data with
     '''
+
     sel = VarianceThreshold(0.8 * (1-0.8))
-    sel.fit(df)
+    return pd.DataFrame(sel.fit_transform(df))
 
 
 
@@ -23,7 +26,13 @@ if __name__ == "__main__":
     X_test = pd.DataFrame.from_csv(test_file)
     train_data = pd.DataFrame.from_csv(train_file)
     #Converting target to numeric variable
-    Y_train = pd.Series([int(x.split('Class_')[1]) for x in train_data['target']])
-    #Getting X_test and X_train
-    X_train = train_data.drop('target',axis=1)
+    Y_data = pd.Series([int(x.split('Class_')[1]) for x in train_data['target']])
+    #Removing target to get X_train
+    X_data = train_data.drop('target',axis=1)
+    X_data = remove_zero_var(X_data)
+    #splitting for cross validation
+    x_train, x_test, y_train, y_test = train_test_split(X_data,Y_data,test_size=0.4)
+    print y_train
+
+
 
