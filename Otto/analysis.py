149e0cd0cd15f9922e71c1546c2d51987026a822
Kartihk Bharadwaj <karthikb@sg.ibm.com>
2015-03-31 15:07:56 +0800

    Changes to pre_process.py. Renamed to analysis.py

--- Otto/analysis.py
+++ Otto/analysis.py
@@ -3,18 +3,14 @@ __authors__ = "Ganesh Karthik"
 '''
 
 import pandas as pd
-from sklearn.feature_selection import VarianceThreshold
 from sklearn.cross_validation import train_test_split
+from scripts import pre_process as pre_pros,feature_selection as FS
 
 
-def remove_zero_var(df):
-    '''
-    The function removes columns with zero variance columns
-    Removes data with
-    '''
 
-    sel = VarianceThreshold(0.8 * (1-0.8))
-    return pd.DataFrame(sel.fit_transform(df))
+
+
+
 
 
 
@@ -29,10 +25,10 @@ if __name__ == "__main__":
     Y_data = pd.Series([int(x.split('Class_')[1]) for x in train_data['target']])
     #Removing target to get X_train
     X_data = train_data.drop('target',axis=1)
-    X_data = remove_zero_var(X_data)
+    X_data = pre_pros.remove_zero_var(X_data)
+    X_reduced = pre_pros.dimension_reduction(X_data,dim=2)
     #splitting for cross validation
     x_train, x_test, y_train, y_test = train_test_split(X_data,Y_data,test_size=0.4)
-    print y_train
 
 
 
